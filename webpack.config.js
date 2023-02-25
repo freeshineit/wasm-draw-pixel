3b5481f1a107549d36e632dd97eb8ed7c7268795
ShineShao <xiaoshaoqq@gmail.com>
2023-02-25 16:47:49 +0800

    upgrade webpack5

--- webpack.config.js
+++ webpack.config.js
@@ -7,49 +7,54 @@ module.exports = (env, argv) => {
   const distPath = path.resolve(__dirname, isProd ? "docs" : "dist");
   return {
     devServer: {
-      contentBase: distPath,
+      static: {
+        directory: distPath,
+      },
       compress: isProd,
-      port: 8000
+      port: 8000,
     },
     entry: "./app/index.ts",
     output: {
       path: distPath,
       filename: "index.js",
-      webassemblyModuleFilename: "index.wasm"
+      webassemblyModuleFilename: "index.wasm",
     },
     module: {
       rules: [
         {
           test: /\.s[ac]ss$/i,
-          use: ["style-loader", "css-loader", "sass-loader"]
+          use: ["style-loader", "css-loader", "sass-loader"],
         },
         {
           test: /\.(ts|js)?$/,
           use: "ts-loader",
-          exclude: /node_modules/
+          exclude: /node_modules/,
         },
         {
           test: /\.(png|jpg|gif|jpeg)$/,
-          loader: "url-loader"
-        }
-      ]
+          loader: "url-loader",
+        },
+      ],
     },
     plugins: [
       new CopyWebpackPlugin({
-        patterns: [{ from: "./public", to: distPath }]
+        patterns: [{ from: "./public", to: distPath }],
       }),
       // We point our WasmPackPlugin to the location of the
       // the crates `Cargo.toml` file. Never the root file.
       new WasmPackPlugin({
         crateDirectory: path.join(__dirname, "wasm"),
-        outDir: path.join(__dirname, "wasm/pkg")
+        outDir: path.join(__dirname, "wasm/pkg"),
         // extraArgs: ""
-      })
+      }),
     ],
-    watch: argv.mode !== "production",
     resolve: {
       // A little overkill for our tutorial but useful.
-      extensions: [".ts", ".tsx", ".js", ".jsx", ".mts", ".mjs"]
-    }
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".mts", ".mjs"],
+    },
+    experiments: {
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+    },
   };
 };
